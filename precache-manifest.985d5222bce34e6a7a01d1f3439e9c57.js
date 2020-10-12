5119e029d66893124136442a813be1d8835adb02
EliteSocialMarketing <72721853+EliteSocialMarketing@users.noreply.github.com>
2020-10-11 22:16:28 -0600

    Updates

---
+++ precache-manifest.985d5222bce34e6a7a01d1f3439e9c57.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d32695abbae68d5be86cbbc7762c46b",
+    "url": "/BurtonMovies/index.html"
+  },
+  {
+    "revision": "02f39a8601690a19b878",
+    "url": "/BurtonMovies/static/js/2.c9995319.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BurtonMovies/static/js/2.c9995319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f93fc5c866f77af6f5",
+    "url": "/BurtonMovies/static/js/main.5d85cc00.chunk.js"
+  },
+  {
+    "revision": "c90ed3ea55c401f90998",
+    "url": "/BurtonMovies/static/js/runtime-main.5acd6751.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/BurtonMovies/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "1145bd81a8ebd33f4fa5ebf7b2ba37c2",
+    "url": "/BurtonMovies/static/media/react-movie-logo.1145bd81.svg"
+  },
+  {
+    "revision": "80e9d8a2ff7fddf4a6dc1309d9e8a3ff",
+    "url": "/BurtonMovies/static/media/search-icon.80e9d8a2.svg"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/BurtonMovies/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/BurtonMovies/precache-manifest.985d5222bce34e6a7a01d1f3439e9c57.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BurtonMovies/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
